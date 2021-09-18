582709e40c5389dd78f1ec40d4e190c2fb53bb1c
EdgarPsrk <erreapesrkmail.com>
2021-09-17 20:32:48 -0500

    array, rango, intervalo, amplitud

--- inicio.py
+++ inicio.py
@@ -1,2 +1,31 @@
-print('Hello my new proyect')
-print('tenesmo un error por shh')
+from math import log
+def intervalo(muestra):
+    a = log(muestra, 10)
+    b = 3.322
+    c= a*b
+    return 1 + c
+
+if __name__ == "__main__":
+    inicio = input('para iniciar coloca si\n')
+
+    while inicio != 'no':
+        largo_contenedor = int(input('¿Que tamaño tiene n?\n'))
+        contenedor = []
+
+        while largo_contenedor != 0:
+            dato = int(input('ingresa el numero\n'))
+            contenedor.append(dato)
+            largo_contenedor -= 1
+
+        print('Estos son tus datos\n', contenedor)
+        contenedor.sort()
+        print('Lista acomodada', contenedor)
+        datom = contenedor[largo_contenedor-1]
+        datoM = contenedor[0]
+
+        rango = datoM - datom
+        k = intervalo(largo_contenedor)
+        amplitud = rango / k
+
+        inicio = input('tienes otro arreglo\n') 
+
--- proof.py
+++ proof.py
@@ -1,9 +1,26 @@
-print('prueba para el primer commit')
+b = 1.5000001
+print(b)
+c = int(b)
+print('numero redondead', c)
 
+b = 1.499999
+print(b)
+c = int(b)
+print('numero redondead', c)
 
+b = 1.00001
+print(b)
+c = int(b)
+print('numero redondead', c)
 
+b = 1.0
+print(b)
+c = int(b)
+print('numero redondead', c)
 
-
-
+b = 1.99999
+print(b)
+c = int(b)
+print('numero redondead', c)
 
 
